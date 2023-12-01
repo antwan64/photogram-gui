23b055d7778aa5a9bf77fcc13a7195918a0fef4b
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-12-01 18:53:47 +0000

    Show Details in Progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.first
+
+    render({ :template => "user_templates/show"})
+
+  end
+
 end
