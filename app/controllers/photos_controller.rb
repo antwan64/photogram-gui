50a950944510b369ee34906cb94396455dc49209
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-12-04 23:19:44 +0000

    Modifications

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -42,13 +42,13 @@ class PhotosController < ApplicationController
       input_caption = params.fetch("input_caption")
       input_owner_id = params.fetch("input_owner_id")
       
-      a_new_photo = Photo.new
-      
-      a_new_photo.image = input_image
-      a_new_photo.caption = input_caption
-      a_new_photo.owner_id = input_owner_id
+      @a_new_photo = Photo.new
 
-      a_new_photo.save
+      @a_new_photo.image = input_image
+      @a_new_photo.caption = input_caption
+      @a_new_photo.owner_id = input_owner_id
+
+      @a_new_photo.save
       
       
 
